cca7b737e600244ef42c4c8fc8fac72e707dc06d
MarsGotBars <marcinm@live.nl>
2025-01-31 23:06:42 +0100

    Removed non-existent form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import Trending from './components/Scroller/Trending.vue'
 import ResultList from './components/ResultList/ResultList.vue'
-import Form from './components/Form/Form.vue'
+
 let data = ref([])
 const fetchMovies = (query) =>
   fetch(
@@ -25,7 +25,6 @@ handleSearch("minecraft")
 
 <template>
   <h1>movie<span>ender</span></h1>
-  <Form/>
   <ResultList :movieData="data" />
 
   <Trending />
